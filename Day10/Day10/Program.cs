6b50b3b8b1cff5328c4c13356d406205e4008ece
Garrett Girod <girodg@hotmail.com>
2023-06-19 10:55:47 -0400

    added comments

--- Day10/Day10/Program.cs
+++ Day10/Day10/Program.cs
@@ -160,23 +160,27 @@
                             build a superhero with the hero data
                             add the superhero to the list
             */
-            List<Superhero> dc2 = new();
+            List<Superhero> dc2 = new();//create a superhero list
             filePath = Path.Combine(directories, dc);
-            if (File.Exists(filePath))
+            if (File.Exists(filePath))//check if the file is there
             {
+                /* open the file
+                   read the data */
                 string dcText = File.ReadAllText(filePath);//Open, Read, Close the file
+                //split to get the array of hero strings
                 string[] dcData = dcText.Split('\n');
-                foreach (var dcItem in dcData)
+                foreach (var dcItem in dcData)//loop over the hero strings
                 {
                     if (string.IsNullOrWhiteSpace(dcItem)) break;
+                    //split the hero string to get the hero data
                     string[] heroData = dcItem.Split(csv);
-                    Superhero hero = new Superhero()
+                    Superhero hero = new Superhero() //build a superhero with the hero data
                     {
                         Name = heroData[0],
                         Secret = heroData[1],
                         Power = Enum.Parse<Powers>(heroData[2])
                     };
-                    dc2.Add(hero);
+                    dc2.Add(hero);//add the superhero to the list
                 }
             }
             foreach (var dcHero in dc2)
@@ -199,30 +203,30 @@
 
 
 
-            /*
-             * Challenge 3:
-                Serialize (write) the list of superheroes to a json file
-            */
+                /*
+                 * Challenge 3:
+                    Serialize (write) the list of superheroes to a json file
+                */
 
 
 
 
-            /*
-                ╔═══════════════╗ 
-                ║ Deserializing ║
-                ╚═══════════════╝ 
+                /*
+                    ╔═══════════════╗ 
+                    ║ Deserializing ║
+                    ╚═══════════════╝ 
 
-                Recreating the objects from the saved state (data) of objects
+                    Recreating the objects from the saved state (data) of objects
 
-            */
+                */
 
 
 
-            /*
-             
-                Challenge 4: deserialize the jla.json file into a list of superheroes
+                /*
 
-            */
+                    Challenge 4: deserialize the jla.json file into a list of superheroes
+
+                */
+            }
         }
-    }
 }
